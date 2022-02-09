c652a68c0cba2d2b7ffd967297667cefd39b40aa
Crazy-Ataman <maksdadapayr@mail.ru>
2022-02-09 22:18:41 +0300

    Added Lab1_java and Lab2_java.

---
+++ Lab1_java/src/com/crazy_ataman/Main.java
@@ -0,0 +1,9 @@
+package com.crazy_ataman;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Display Hello World!
+        System.out.println("Hello World!");
+    }
+}
---
+++ Lab2_java/src/com/crazy_ataman/Main.java
@@ -0,0 +1,8 @@
+package com.crazy_ataman;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//TODO Example of todo.
+    }
+}
