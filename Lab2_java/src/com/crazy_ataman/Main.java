620f6c337c62d5162d0af3e8d6a2a2a98f36a43f
Crazy-Ataman <maksdadapayr@mail.ru>
2022-02-11 15:00:13 +0300

    Lab2_java is done

--- Lab2_java/src/com/crazy_ataman/Main.java
+++ Lab2_java/src/com/crazy_ataman/Main.java
@@ -1,8 +1,20 @@
 package com.crazy_ataman;
 
+import by.belstu.it.dashchynski.TextFunction;
+
 public class Main {
 
     public static void main(String[] args) {
 	//TODO Example of todo.
+        TextFunction obj = new TextFunction();
+
+        for (int i=0; i<9 ;i++)
+            if (i > -1)
+                if (i < 10)
+                    System.out.println(i);
+
+        for (int count = 0; count < 10; count++) {
+            System.out.println("Counter "+count);
+        }
     }
 }
